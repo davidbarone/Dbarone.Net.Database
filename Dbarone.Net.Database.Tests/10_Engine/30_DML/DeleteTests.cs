f97c0b6735e0bdddfbc9884d68b2ade60a3e8866
davidbarone <davidbarone@live.com>
2022-11-13 12:10:10 +1100

    Delete tests.

--- Dbarone.Net.Database.Tests/10_Engine/30_DML/DeleteTests.cs
+++ Dbarone.Net.Database.Tests/10_Engine/30_DML/DeleteTests.cs
@@ -7,47 +7,33 @@ using System.Collections.Generic;
 
 public class DeleteTests : TestBase
 {
-    [Fact]
-    public void TestDelete()
+    [Theory]
+    [InlineData("PersonId", 1, 1)]
+    [InlineData("PersonId", -1, 0)]
+    [InlineData("Comments", "XXX", 3)]
+    public void Test_Delete(string columnName, object value, int expectedRowsAffected)
     {
         // Arrange
         var dbName = GetDatabaseFileNameFromMethod();
-        if (File.Exists(dbName))
-        {
-            File.Delete(dbName);
-        }
-        var tableName = "Post";
+        int rowsAffected = 0;
+        TableInfo? table = null;
 
-        // Act
-        using (var db = Engine.Create(dbName))
+        using (var db = CreateDatabaseWithOverwriteIfExists(dbName))
         {
-            ColumnInfo[] columns = {
-                new ColumnInfo("PostId", DataType.Int32, false),
-                new ColumnInfo("PostTitle", DataType.String, false)
-            };
-
-            db.CreateTable(tableName, columns);
-            db.Insert("Post", new Dictionary<string, object?>{
-                {"PostId", 1},
-                {"PostTitle", "First Post"}
-            });
-            db.Insert("Post", new Dictionary<string, object?>{
-                {"PostId", 2},
-                {"PostTitle", "Second Post"}
-            });
-            // Delete post #2
-            db.DeleteRaw("Post", (row) => { return (int?)(row!["PostId"])==2 ? true : false; });
+            table = CreateTableFromEntity<PersonInfo>(db);
+            var people = PersonInfo.CreateTestData();
+            db.BulkInsert<PersonInfo>(table.TableName, people);
+            rowsAffected = db.DeleteRaw(table.TableName, (row) => { return (row![columnName])!.Equals(value) ? true : false; });
             db.CheckPoint();    // Save pages to disk
         }
 
-        // Assert
         using (var db = Engine.Open(dbName, false))
         {
             // Assert
             var tables = db.Tables();
             Assert.Single(tables);                    // 1 table
-            Assert.Equal(tableName, tables.First().TableName);
-            Assert.Single(db.ReadRaw(tableName));
+            Assert.Equal(expectedRowsAffected, rowsAffected);
+            Assert.Equal(10 - expectedRowsAffected, db.ReadRaw(table.TableName).Count());
         }
     }
 }
