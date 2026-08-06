47b595aa423e7a708d19e66cb62f07c4ebea2b7c
davidbarone <davidbarone@live.com>
2026-08-06 12:21:20 +1000

    wip

--- Dbarone.Net.Database.Tests/Serialization/Parquet/TestPack/TestPackColumn.cs
+++ Dbarone.Net.Database.Tests/Serialization/Parquet/TestPack/TestPackColumn.cs
@@ -17,7 +17,7 @@ using Dbarone.Net.Extensions;
 /// </summary>
 public class TestPackColumn
 {
-  public TestPackColumn(Type dataType, bool? nullable = null, Func<IEnumerable<object>> generator)
+  public TestPackColumn(Type dataType, Func<IEnumerable<object>> generator, bool? nullable = null)
   {
     this.DataType = dataType;
     this.Generator = generator;
